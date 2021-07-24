14b2873a9359ce25af3e43ca53bf4be67c249d48
Lorenzo Zoli <zoli.lorenzo95@gmail.com>
2021-07-24 13:29:21 +0200

    First interaction w/Metamask

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Web3 from 'web3';
+import { Eth } from 'web3-eth';
 
-function App() {
+export interface CustomGlobalThis {
+  [any: string]: any;
+}
+
+const App: React.FC<any> = () => {
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+  const [connectedAddress, setAddress] = useState<string | null>(null);
+  const [ethInstance, SetEthInstance] = useState<Eth | null>(null);
+  const [balance, setBalance] = useState<string>('')
+
+  // Inialize empty web3
+  let web3: Web3 = new Web3();
+
+  useEffect(() => {
+    tryConnect();
+    getAccountBalances();
+  }, []);
+
+  const tryConnect = async () => {
+    if ('ethereum' in window) {
+      web3 = new Web3((window as CustomGlobalThis).ethereum);
+      try {
+        // Request access
+        await (window as CustomGlobalThis).ethereum.enable();
+        SetEthInstance(web3.eth);
+        setIsConnected(true);
+        setAddress((window as CustomGlobalThis).ethereum.selectedAddress);
+      } catch (e) {
+        setIsConnected(false);
+        setAddress(null);
+        alert('Metamask not found');
+        return;
+      }
+    }
+  }
+
+  const getAccountBalances = async () => {
+    if (isConnected && ethInstance && connectedAddress) {
+      const balance = await ethInstance.getBalance(connectedAddress);
+      setBalance(Web3.utils.fromWei(balance, 'ether'))
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isConnected ?
+        (
+          <div>
+            Address: {connectedAddress}
+            <br></br>
+            Balance: { balance }
+          </div>
+        ):
+        (
+          <button style={{ marginTop: 50 }} onClick={() => tryConnect()}>
+            Connect metamask
+          </button>
+        )}
     </div>
-  );
+  )
 }
 
 export default App;
